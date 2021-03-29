625dbf63d7254ed6e5404fc72e675ca0e4647ee8
hsaturn <hsaturn@gmail.com>
2021-03-29 22:19:20 +0200

    wificlient & server

--- src/ESP8266WiFi.h
+++ src/ESP8266WiFi.h
@@ -1,2 +1,16 @@
+// vim: ts=2, sw=2
 #pragma once
 #include <Esp8266.h>
+
+class WiFiClient
+{
+	public:
+		void write(const char* p, uint32_t length);
+};
+
+class WiFiServer
+{
+	public:
+		void begin();
+		uint16_t port();
+};
