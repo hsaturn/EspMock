63a188bde4f7cdf9af1670bc5984407b30b512a3
hsaturn <hsaturn@gmail.com>
2021-03-30 00:02:09 +0200

    update

--- src/ESP8266WiFi.h
+++ src/ESP8266WiFi.h
@@ -5,22 +5,28 @@
 class WiFiClient
 {
 	public:
-		void write(const void * p, unsigned int length);
-		bool connected() const;
-		bool connect(const char* host, uint16_t port);
-		void stop();
-		bool available();
+		void write(const void * p, unsigned int length) {};
+		bool connected() const { return connected_; }
+		bool connect(const char* host, uint16_t port) { return false; };
+		void stop() {};
+		bool available() { return false; }
 
-		char read();
+		char read() { return 0; };
 
-		operator bool() const;
+		operator bool() const { return false; };
+
+	private:
+		bool connected_ = false;
 };
 
 class WiFiServer
 {
 	public:
-		WiFiServer(const unsigned int);
-		void begin();
-		int port() const;
-		WiFiClient available();
+		WiFiServer(const unsigned int) {};
+		void begin() {};
+		int port() const { return port_; };
+		WiFiClient available() { return WiFiClient(); }
+
+	private:
+		int port_;
 };
