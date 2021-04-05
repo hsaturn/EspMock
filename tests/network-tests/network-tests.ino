df097f62f3c4f842e00350569c7e974a841175bd
hsaturn <hsaturn.gmail.com>
2021-04-05 17:15:44 +0200

    Unit tests

---
+++ tests/network-tests/network-tests.ino
@@ -0,0 +1,44 @@
+// vim: ts=2 sw=2 expandtab
+#include <AUnit.h>
+#include <ESP8266WiFi.h>
+
+/**
+  * EspMock netword unit tests.
+  *
+  * No wifi connection unit tests.
+  * Checks with a local broker. Clients must connect to the local broker
+  **/
+
+using namespace std;
+
+test(network_wificlient_should_return_connected_when_connected_byname)
+{
+    WiFiClient client;
+    assertFalse(client.connected());
+    client.connect("www.google.com", 80);
+    assertTrue(client.connected());
+}
+
+test(network_wificlient_should_return_connected_when_connected_byipaddress)
+{
+  IPAddress ip{127,0,0,1};
+  WiFiClient client;
+  assertFalse(client.connected());
+  client.connect(ip, 80);
+  assertTrue(client.connected());
+}
+
+test(network_wificlient_should_not_return_connected_when_not_connected)
+{
+    WiFiClient client;
+    assertFalse(client.connected());
+    client.connect("cannot-connect-there-because-of-wrong-domain.com", 80);
+    assertFalse(client.connected());
+}
+
+void setup() {
+}
+
+void loop() {
+  aunit::TestRunner::run();
+}
